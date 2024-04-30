d8a0a36a9ede7b8cdb166d0085a74d85b4d8b24a
sayka88 <sayka.alekperova@gmail.com>
2024-04-30 16:38:43 +0000

    betty.c new

--- betty.c
+++ betty.c
@@ -1,18 +1,24 @@
 #include "main.h"
 
 /**
-* print_hello - prints "Hello, Betty!"
-*
-* Return: Always 0
-*/
+ * print_hello - prints "Hello, Betty!"
+ *
+ * Return: Always 0
+ */
 int print_hello(void)
 {
-	char *str = "Hello, Betty!\n";
+    char *str = "Hello, Betty!\n";
 
-	_putchar(*str);
-	while (*(++str))
-	{
-		_putchar(*str);
-	}
-	return (0);
+    while (*str)
+    {
+        _putchar(*str);
+        str++;
+    }
+    return (0);
+}
+
+int main(void)
+{
+    print_hello();
+    return (0);
 }
