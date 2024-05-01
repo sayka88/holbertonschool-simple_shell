c0638ea27e474ef3da785a094e6c54508f407ccb
AysuAnar <8921@holbertonstudents.com>
2024-05-01 13:12:54 +0000

    first version

--- execute.c
+++ execute.c
@@ -1,125 +1,95 @@
 #include "main.h"
-
-/*char *fullpath;*/
-
 /**
- * printerror - prints error
- * @command: command inserted
+ * execute - executes command
+ *
+ *@command: The command string to execute.
+ *
+ * Return: the exit status of the executed command,
+ * or -1 if an error occurs.
  */
-
-void printerror(char *const command[])
+int execute(char *command)
 {
-	fprintf(stderr, "./hsh: 1: %s: not found\n", command[0]);
-	free(*command);
-	exit(127);
-}
+        int status = 0;
+        pid_t pid = fork();
 
-/**
- * _getenv - get environment variable
- * @name: name of environment variable
- * @envp: pointer to environment variables
- * Return: pointer to environment variable
- */
+        if (pid == -1)
+        {
+                perror("fork");
+                free(command);
+                exit(EXIT_FAILURE);
+        }
+        else if (pid == 0)
+        {
+                char *arr[64];
+                line_div(command, arr);
+                if (arr[0] == NULL)
+                {
+                        free(command);
+                        exit(EXIT_SUCCESS);
+                }
+                if (strcmp(arr[0], "env") == 0)
+                {
+                        char **env = environ;
 
-char *_getenv(const char *name, char **envp)
-{
-	char **env, *separator;
-
-	for (env = envp; *env != NULL; env++)
-	{
-		separator = strchr(*env, '=');
-		if (separator != NULL)
-		{
-			if (strncmp(*env, name, separator - *env) == 0)
-			{
-				return (separator + 1);
-			}
-		}
-	}
-	return (NULL);
-}
-
-/**
- * pathfinder - finds the correct path for given alias
- * @command: given input to check for path
- * @cmd: argument
- * @envp: pointer to environment variables
- * Return: pointer to the string array or NULL if failed
- */
-
-char **pathfinder(char *cmd, char **command, char **envp)
-{
-	char *current_path, *temp_path;
-	char *path_tok;
-	size_t arglen = strlen(cmd);
-
-	fullpath = NULL;
-
-	if (strchr(cmd, '/') != NULL && access(cmd, F_OK) == 0)
-	{
-		command[0] = cmd;
-		return (command);
-	}
-
-	path_tok = NULL;
-	current_path = _getenv("PATH", envp);
-	temp_path = strdup(current_path);
-	path_tok = strtok(temp_path, ":");
-
-	while (path_tok)
-	{
-		fullpath = malloc(arglen + strlen(path_tok) + 2);
-		sprintf(fullpath, "%s/%s", path_tok, cmd);
-		if (access(fullpath, F_OK) == 0)
-		{
-			command[0] = fullpath;
-			free(temp_path);
-			return (command);
-		}
-		path_tok = strtok(NULL, ":");
-		free(fullpath);
-	}
-	free(temp_path);
-	return (NULL);
-}
-
-/**
- * execute - function to execute commands
- * @command: input froom user
- * @envp: enviroment path
- * Return: -1 if failed and 0 if success
- */
-
-int execute(char *const command[], char **envp)
-{
-	pid_t id;
-	int status, i;
-	char **temp = pathfinder(command[0], (char **) command, envp);
-
-	if (temp != NULL)
-	{
-		id = fork();
-		if (id < 0)
-		{
-			perror("fork failed");
-			return (-1);
-		} else if (id == 0)
-		{
-			if (_getenv("PATH", envp) == NULL && access(command[0], F_OK) != 0)
-				printerror(command);
-			execve(temp[0], command, envp);
-			for (i = 0; command[i] != NULL; i++)
-				free(command[i]);
-			exit(EXIT_FAILURE);
-		}
-		wait(&status);
-		free(fullpath);
-	} else
-		printerror(command);
-	if (errno == -1)
-	{
-		free(*command);
-		exit(2);
-	}
-	return (0);
+                        while (*env != NULL)
+                        {
+                                printf("%s\n", *env);
+                                env++;
+                        }
+                        free(command);
+                        exit(EXIT_SUCCESS);
+                }
+                if (strchr(arr[0], '/') != NULL)
+                {
+                        if (access(arr[0], X_OK) == 0)
+                        {
+                                if (execve(arr[0], arr, environ) == -1)
+                                {
+                                        perror("execve");
+                                        free(command);
+                                        exit(EXIT_FAILURE);
+                                }
+                        }
+                }
+                else
+                {
+                        char *path = getenv("PATH");
+                        char *token;
+                        if (path == NULL)
+                        {
+                                fprintf(stderr, "./hsh: 1: %s: not found\n", arr[0]);
+                                free(command);
+                                exit(127);
+                        }
+                        token = strtok(path, ":");
+                        while (token != NULL)
+                        {
+                                char executable_path[256];
+                                snprintf(executable_path, sizeof(executable_path), "%s/%s", token, arr[0]);
+                                if (access(executable_path, X_OK) == 0)
+                                {
+                                        if (execve(executable_path, arr, environ) == -1)
+                                        {
+                                                perror("execve");
+                                                free(command);
+                                                exit(EXIT_FAILURE);
+                                        }
+                                }
+                                token = strtok(NULL, ":");
+                        }
+                }
+                fprintf(stderr, "./hsh: 1: %s: not found\n", arr[0]);
+                free(command);
+                exit(127);
+        }
+        else
+        {
+                waitpid(pid, &status, 0);
+                free(command);
+                if (WIFEXITED(status))
+                        status = WEXITSTATUS(status);
+                else
+                        status = 1;
+        }
+        return (status);
 }
--- main.h
+++ main.h
@@ -5,15 +5,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-
-int execute(char *const command[], char **envp);
-int print_env(void);
-char *_getenv(const char *name, char **envp);
-void parse(char command[], char **envp);
-
-#endif 
+#include <fcntl.h>
+char **line_div(char *command,char **arr);
+char *_getline(void);
+int execute(char *command);
+extern char **environ;
+int main(void);
+#endif
