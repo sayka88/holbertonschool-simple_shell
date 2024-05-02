6ccbe069534d38e246c404d4f20908b34107e8ea
sayka88 <sayka.alekperova@gmail.com>
2024-05-02 12:09:40 +0000

    execute.c

--- execute.c
+++ execute.c
@@ -1,59 +1,40 @@
 #include "main.h"
-/**
- * _err - checks and handles errors
- * @args: arguments to check
- * Return: void
- */
-
-void _err(char *args[])
-{
-	fprintf(stderr, "%s: command not found\n", args[0]);
-	perror("");
-	free(args[0]);
-	exit(98);
-}
-
-
-/**
- * exec - executes the input received
- * @args: arguments
- * @input: input
- * Return: void
- */
-
 void exec(char **args, char *input)
 {
-
+	pid_t childPid;
 	int status;
-	pid_t childPid = 0;
-
-	if (access(args[0], X_OK) != 0)
-		_err(args);
 
 	childPid = fork();
 
 	if (childPid == -1)
 	{
-		perror("fork\n");
+		perror("fork");
 		free(input);
-		free(args[0]);
 		exit(EXIT_FAILURE);
-	}
-	else if (childPid == 0)
-	{
-		execve(args[0], args, environ);
-		perror(args[0]);
-		free(args[0]);
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		wait(&status);
+	} else if (childPid == 0) {
+		if (execvp(args[0], args) == -1)
+		{
+			perror(args[0]);
+			free(input);
+			exit(EXIT_FAILURE);
+		}
+	} else {
+
+		if (waitpid(childPid, &status, 0) == -1)
+		{
+			perror("waitpid");
+			free(input);
+			exit(EXIT_FAILURE);
+		}
+		
 		if (WIFEXITED(status))
 		{
-			free(args[0]);
 			free(input);
 			exit(WEXITSTATUS(status));
+		} else {
+			free(input);
+			exit(EXIT_FAILURE);
 		}
 	}
 }
+
