ee44008e3c7191134eb844a269b12d2265a4a8e0
sayka88 <sayka.alekperova@gmail.com>
2024-05-02 11:14:15 +0000

    updated all files

--- main.h
+++ main.h
@@ -12,7 +12,7 @@
 extern char **environ;
 
 void exec(char **args, char *input);
-void tokenize(char *input, char *args[]);
+void tokenize(char *input, char **args);
 void handlePath(const char* path, int length);
 void printEnv(void);
 char *handle_path(char *input);
