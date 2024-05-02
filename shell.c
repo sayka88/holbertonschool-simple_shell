76b6ec4a4bc6352c8fc751a6479dcaa6c297576d
sayka88 <sayka.alekperova@gmail.com>
2024-05-02 06:18:09 +0000

    update shell.c

--- shell.c
+++ shell.c
@@ -7,36 +7,35 @@
  */
 int main(void)
 {
-    char *command;
-    int status;
+	char *command;
+	int status;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-            printf("$ ");
-        command = _getline();
-        if (command == NULL)
-            break;
-        if (strcmp(command, "exit") == 0)
-        {
-            free(command);
-            exit(0);
-        }
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("$ ");
+		command = _getline();
+		if (command == NULL)
+			break;
+		if (strcmp(command, "exit") == 0)
+		{
+			free(command);
+			exit(0);
+		}
 
-        // Check if the command exists
-        if (access(command, X_OK) == -1)
-        {
-            printf("Command not found: %s\n", command);
-            free(command);
-            continue;
-        }
+		if (access(command, X_OK) == -1)
+		{
+			printf("Command not found: %s\n", command);
+			free(command);
+			continue;
+		}
 
-        status = execute(command);
-        if (status == 2)
-        {
-            exit(2);
-        }
-    }
-    return (status);
+		status = execute(command);
+		if (status == 2)
+		{
+			exit(2);
+		}
+	}
+	return (status);
 }
 
