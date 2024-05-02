a871d5a74e779de5bcc64b5321006d659242b7bb
sayka88 <sayka.alekperova@gmail.com>
2024-05-02 10:08:01 +0000

    execute.c new

--- execute.c
+++ execute.c
@@ -1,17 +1,25 @@
 #include "main.h"
 
 /**
- * execute - executes command
- *
- * @command: The command string to execute.
- *
- * Return: the exit status of the executed command,
- * or -1 if an error occurs.
- */
+* execute - executes command
+*
+* @command: The command string to execute.
+*
+* Return: the exit status of the executed command,
+* or -1 if an error occurs.
+*/
 int execute(char *command)
 {
 	int status = 0;
-	pid_t pid = fork();
+	pid_t pid;
+
+	if (access(command, X_OK) == -1)
+	{
+		fprintf(stderr, "Function 'fork' is not allowed\n");
+		return 1;
+	}
+
+	pid = fork();
 
 	if (pid == -1)
 	{
@@ -23,65 +31,12 @@ int execute(char *command)
 	{
 		char *arr[64];
 		line_div(command, arr);
-		if (arr[0] == NULL)
-		{
-			free(command);
-			exit(EXIT_SUCCESS);
-		}
-		if (strcmp(arr[0], "env") == 0)
-		{
-			char **env = environ;
 
-			while (*env != NULL)
-			{
-				printf("%s\n", *env);
-				env++;
-			}
-			free(command);
-			exit(EXIT_SUCCESS);
-		}
-		if (strchr(arr[0], '/') != NULL)
-		{
-			if (access(arr[0], X_OK) == 0)
-			{
-				if (execve(arr[0], arr, environ) == -1)
-				{
-					perror("execve");
-					free(command);
-					exit(EXIT_FAILURE);
-				}
-			}
-		}
-		else
-		{
-			char *path = getenv("PATH");
-			char *token;
-			if (path == NULL)
-			{
-				fprintf(stderr, "./hsh: 1: %s: not found\n", arr[0]);
-				free(command);
-				exit(127);
-			}
-			token = strtok(path, ":");
-			while (token != NULL)
-			{
-				char executable_path[256];
-				snprintf(executable_path, sizeof(executable_path), "%s/%s", token, arr[0]);
-				if (access(executable_path, X_OK) == 0)
-				{
-					if (execve(executable_path, arr, environ) == -1)
-					{
-						perror("execve");
-						free(command);
-						exit(EXIT_FAILURE);
-					}
-				}
-				token = strtok(NULL, ":");
-			}
-		}
-		fprintf(stderr, "./hsh: 1: %s: not found\n", arr[0]);
+		execve(arr[0], arr, environ);
+
+		perror("execve");
 		free(command);
-		exit(127);
+		exit(EXIT_FAILURE);
 	}
 	else
 	{
@@ -92,5 +47,6 @@ int execute(char *command)
 		else
 			status = 1;
 	}
-	return (status);
+	return status;
 }
+
