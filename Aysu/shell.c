3374084755ee1ed32792889ed6e4178b056ec1ed
sayka88 <sayka.alekperova@gmail.com>
2024-05-03 10:45:55 +0000

    new files

---
+++ Aysu/shell.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * main - main func
+ *
+ * Return: int
+ */
+int main(void)
+{
+	char *command;
+	int status;
+	
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("$ ");
+		command = _getline();
+		if (command == "")
+			break;
+		if (strcmp(command, "exit") == 0)
+		{
+			free(command);
+			exit(0);
+		}
+		status = execute(command);
+		if (status == 2)
+		{
+			exit(2);
+		}
+	}
+	return (status);
+}
--- shell.c
+++ shell.c
@@ -1,32 +1,56 @@
+#define _GNU_SOURCE
+
 #include "main.h"
+
 /**
- * main - main func
- *
- * Return: int
+ * spacesCheck - check if str contain only space
+ * @str: string to check
+ * Return: 0 on success or 1 on failure
  */
+
+int spacesCheck(const char *str)
+{
+	while (*str)
+	{
+		if (*str != ' ')
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+/**
+ * main - main function for the shell
+ * Return: 0 on success
+ */
+
 int main(void)
 {
-	char *command;
-	int status;
-	
+	char *input = NULL;
+	char *args[64] = { NULL };
+	size_t inputSize = 0;
+	ssize_t inputRead;
+
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-			printf("$ ");
-		command = _getline();
-
-		if (command == NULL)
-			break;
-		if (strcmp(command, "exit") == 0)
 		{
-			free(command);
+			printf("$ ");
+			fflush(stdout);
+		}
+
+		inputRead = getline(&input, &inputSize, stdin);
+		if (inputRead == EOF)
+		{
+			free(input);
 			exit(0);
 		}
-		status = execute(command);
-		if (status == 2)
-		{
-			exit(2);
-		}
+
+		if (inputRead > 0 && input[inputRead - 1] == '\n')
+			input[inputRead - 1] = '\0';
+		if (spacesCheck(input) != 1)
+			tokenize(input, args);
 	}
-	return (status);
+	free(input);
+	return (0);
 }
