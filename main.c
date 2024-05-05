171b317b1bf42382f92a5b35c8cbb7a90b3ad834
AysuAnar <8921@holbertonstudents.com>
2024-05-05 11:07:31 +0000

    main.c fix bugs

--- main.c
+++ main.c
@@ -1,52 +1,57 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-* main - Creates an interactive loop in which the
-*        user can enter commands and receive responses
-*        from the program.
-* Return: Always 0.
-*/
-
-int main(void)
+ * main - Creates an interactive loop in which the
+ *        user can enter commands and receive responses
+ *        from the program.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
 {
-	ssize_t read;
-	char *input = NULL;
-	size_t len = 0;
-	int i;
+    ssize_t read;
+    char *input = NULL;
+    size_t len = 0;
+    int i;
 
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			printf("#cisfun$ ");
+    printf("Received %d arguments:\n", argc);
+    for (i = 0; i < argc; i++) {
+        printf("Argument %d: %s\n", i, argv[i]);
+    }
 
-		read = getline(&input, &len, stdin);
-		if (read == -1)
-		{
-			free(input);
-			exit(0);
-		}
+    while (1)
+    {
+        if (isatty(STDIN_FILENO))
+            printf("#cisfun$ ");
 
-		input[read - 1] = '\0';
+        read = getline(&input, &len, stdin);
+        if (read == -1)
+        {
+            free(input);
+            exit(0);
+        }
 
-		for (i = 0; input[i] != '\0'; i++)
-		{
-			if (input[i] != ' ')
-				break;
-		}
+        input[read - 1] = '\0';
 
-		if (input[i] == '\0')
-			continue;
+        for (i = 0; input[i] != '\0'; i++)
+        {
+            if (input[i] != ' ')
+                break;
+        }
 
-		if (strcmp(input, "exit") == 0)
-		{
-			free(input);
-			exit(0);
-		}
-		else if (strcmp(input, "env") == 0)
-			printEnvironment();
-		else
-			executeCommand(input);
-	}
-	free(input);
-	return (0);
+        if (input[i] == '\0')
+            continue;
+
+        if (strcmp(input, "exit") == 0)
+        {
+            free(input);
+            exit(0);
+        }
+        else if (strcmp(input, "env") == 0)
+            printEnvironment();
+        else
+            executeCommand(input);
+    }
+    free(input);
+    return (0);
 }
