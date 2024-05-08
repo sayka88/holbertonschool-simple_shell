1b8a9ad22e0e396141ada65dce289a5ede6e0fd2
sayka88 <sayka.alekperova@gmail.com>
2024-05-08 22:25:58 +0000

    resolve /bin/ls bug

--- exec_exists_cmd.c
+++ exec_exists_cmd.c
@@ -1,59 +1,55 @@
 #include "main.h"
-/**
- * _err - checks and handles errors
- * @args: arguments to check
- * Return: void
- */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 void _err(char *args[])
 {
-	fprintf(stderr, "%s: command not found\n", args[0]);
-	perror("");
-	free(args[0]);
-	exit(98);
+    fprintf(stderr, "%s: command not found\n", args[0]);
+    perror("");
+    free(args[0]);
+    exit(98);
 }
-
-
-/**
- * exec - executes the input received
- * @args: arguments
- * @input: input
- * Return: void
- */
 
 void exec(char **args, char *input)
 {
+    int status;
+    pid_t childPid = 0;
 
-	int status;
-	pid_t childPid = 0;
+    struct stat st;
+    if (stat(args[0], &st) == -1) {
+        _err(args);
+    }
 
-	if (access(args[0], X_OK) != 0)
-		_err(args);
+    if (!S_ISREG(st.st_mode) || (st.st_mode & S_IXUSR) == 0) {
+        _err(args);
+    }
 
-	childPid = fork();
+    childPid = fork();
 
-	if (childPid == -1)
-	{
-		perror("fork\n");
-		free(input);
-		free(args[0]);
-		exit(EXIT_FAILURE);
-	}
-	else if (childPid == 0)
-	{
-		execve(args[0], args, environ);
-		perror(args[0]);
-		free(args[0]);
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		wait(&status);
-		if (WIFEXITED(status))
-		{
-			free(args[0]);
-			free(input);
-			exit(WEXITSTATUS(status));
-		}
-	}
+    if (childPid == -1)
+    {
+        perror("fork\n");
+        free(input);
+        free(args[0]);
+        exit(EXIT_FAILURE);
+    }
+    else if (childPid == 0)
+    {
+        execve(args[0], args, environ);
+        perror(args[0]);
+        free(args[0]);
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        wait(&status);
+        if (WIFEXITED(status))
+        {
+            free(args[0]);
+            free(input);
+            exit(WEXITSTATUS(status));
+        }
+    }
 }
+
--- tokenize.c
+++ tokenize.c
@@ -9,43 +9,34 @@
 
 void tokenize(char *input, char *args[])
 {
-	char *token;
-	unsigned int i = 0;
+    char *token;
+    char *executable;
+    unsigned int i = 0;
 
-	token = strtok(input, " ");
-	while (token != NULL)
-	{
-		args[i] = token;
-		i++;
-		token = strtok(NULL, " ");
-	}
-	args[i] = NULL;
+    token = strtok(input, " ");
+    while (token != NULL)
+    {
+        args[i] = token;
+        i++;
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL;
 
-	if (args[0] == NULL)
-		exit(0);
+    if (args[0][0] == '/')
+    {
+        args[0] = strdup(args[0]);
+        return;
+    }
 
-	if (strcmp(input, "env") == 0)
-	{
-		printEnv();
-		return;
-	}
-
-	if (strcmp(input, "exit") == 0 && args[1] == NULL)
-	{
-		free(args[0]);
-		exit(0);
-	}
-
-	token = strdup(args[0]);
-	args[0] = handle_path(args[0]);
-	if (args[0] != NULL)
-	{
-		free(token);
-		exec(args, input);
-		free(args[0]);
-		return;
-	}
-	fprintf(stderr, "./hsh: 1: %s: not found\n", token);
-	free(token);
-	exit(127);
+    executable = handle_path(args[0]);
+    if (executable != NULL)
+    {
+        args[0] = executable;
+    }
+    else
+    {
+        fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+        exit(127);
+    }
 }
+
