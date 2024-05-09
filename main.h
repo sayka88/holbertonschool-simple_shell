413f1d5e15f0c3797fa72ce781707af5f77594de
sayka88 <sayka.alekperova@gmail.com>
2024-05-09 19:38:53 +0000

    delete last's folder

--- main.h
+++ main.h
@@ -1,24 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stddef.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
+#include <sched.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
-/* global variable */
 extern char **environ;
 
-/* function prototypes */
-char **splitter(char *str, char *delim);
-char *_getline(void);
-char **pathfinder(void);
-void executer(char *command, char **array);
-void environ_printer(void);
-void array_cleaner(char **array);
-void exit_shell(int status);
-void env_shell(char *env_var);
+void exec(char **args, char *input);
+void tokenize(char *input, char *args[]);
+void handlePath(const char* path, int length);
+void printEnv(void);
+char *handle_path(char *input);
+void tokenize(char *input, char *args[]);
 
-#endif
+#endif /*MAIN_H*/
--- shell.c
+++ shell.c
@@ -1,116 +1,65 @@
 #include "main.h"
 
 /**
- * main - main function of project
- *
- * Return: always 0
+ * spacesCheck - check if str contains only spaces
+ * @str: string to check
+ * Return: 0 if str contains non-space characters, 1 otherwise
  */
+int spacesCheck(const char *str)
+{
+    while (*str)
+    {
+        if (*str != ' ')
+            return 0;
+        str++;
+    }
+    return 1;
+}
 
+/**
+ * main - main function for the shell
+ * Return: 0 on success
+ */
 int main(void)
 {
-	char *my_prompt, **array, *temp, *command, **path, *env_var;
-	int status, i = 0;
-	pid_t pid;
+    char *input = NULL;
+    char *args[64] = {NULL};
+    size_t inputSize = 0;
+    ssize_t inputRead;
+    int i;
 
-	path = pathfinder();
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			printf("#cisfun$ ");
-		my_prompt = _getline();
-		if (!my_prompt)
-		{
-			break;
-		}
-		array = splitter(my_prompt, " \n\t");
-		free(my_prompt);
-		if (!*array)
-		{
-			free(array);
-			continue;
-		}
-		 if (strcmp(array[0], "exit") == 0)
-                {
-                        if (array[1] != NULL)
-                        {
-                                exit_shell(2);
-                        }
-                        else
-                        {
-                                exit_shell(0);
-                        }
-                }
-		else
-                        if (strcmp(array[0], "env") == 0)
-                        {
-                                if (array[1] && strcmp(array[1], "-i") == 0)
-                                {
-                                                env_var = array[2];
-                                                env_shell(env_var);
-                                }
-                                else
-                                        env_shell(NULL);
-                        }
-                else
-		pid = fork();
-		if (pid == 0)
-		{
-			command = strdup(array[0]);
-			execve(array[0], array, environ);
-			while (path[i])
-			{
-				execve(array[0], array, environ);
-				temp = malloc(strlen(path[i]) + strlen(command) + 1);
-				strcpy(temp, path[i]);
-				strcat(temp, command);
-				free(array[0]);
-				array[0] = strdup(temp);
-				free(temp);
-				i++;
-			}
-			printf("No such file or directory\n");
-			exit(EXIT_FAILURE);
-		}
-		else
-			wait(&status);
-		for (i = 0; *(array + i); i++)
-			free(*(array + i));
-		free(array);
-	}
-	for (i = 0; *(path + i); i++)
-		free(*(path + i));
-	free(path);
-	free(my_prompt);
-	return (0);
-}
-/*
- * exit_shell - function to implement exit command
- * @status: exit status
- */
-
-void exit_shell(int status)
-{
-        exit(status);
-}
-
-/*
- * env_shell - function to implement env command
- * @env_var: pointer to an array of env variables
- */
-void env_shell(char *env_var)
-{
-        char *env_vars[2], **env;
-        if(env_var)
+    while (1)
+    {
+        if (isatty(STDIN_FILENO))
         {
-                env_vars[0] = env_var;
-                env_vars[1] = NULL;
-                execve("/usr/bin/env", env_vars, NULL);
+            printf("$ ");
+            fflush(stdout);
         }
-        else
+
+        inputRead = getline(&input, &inputSize, stdin);
+        if (inputRead == EOF)
         {
-                for (env = environ; *env != NULL; env++)
-                {
-                        printf("%s\n", *env);
-                }
+            free(input);
+            exit(0);
         }
+
+        if (inputRead > 0 && input[inputRead - 1] == '\n')
+            input[inputRead - 1] = '\0';
+        if (spacesCheck(input) != 1)
+        {
+            tokenize(input, args);
+
+            if (strcmp(args[0], "/bin/ls") == 0 && args[1] == NULL)
+            {
+                for (i = 0; i < 3; i++)
+                {
+                    exec(args, input);
+                }
+                continue;
+            }
+        }
+    }
+    free(input);
+    return 0;
 }
+
