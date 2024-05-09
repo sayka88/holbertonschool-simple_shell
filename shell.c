690b88c621b1a3bed78ea569f96b5547ae88502f
sayka88 <sayka.alekperova@gmail.com>
2024-05-09 19:45:25 +0000

    exec file

--- shell.c
+++ shell.c
@@ -1,65 +1,57 @@
+#define _GNU_SOURCE
+
 #include "main.h"
 
 /**
- * spacesCheck - check if str contains only spaces
+ * spacesCheck - check if str contain only space
  * @str: string to check
- * Return: 0 if str contains non-space characters, 1 otherwise
+ * Return: 0 on success or 1 on failure
  */
+
 int spacesCheck(const char *str)
 {
-    while (*str)
-    {
-        if (*str != ' ')
-            return 0;
-        str++;
-    }
-    return 1;
+	while (*str)
+	{
+		if (*str != ' ')
+			return (0);
+		str++;
+	}
+	return (1);
 }
 
 /**
  * main - main function for the shell
  * Return: 0 on success
  */
+
 int main(void)
 {
-    char *input = NULL;
-    char *args[64] = {NULL};
-    size_t inputSize = 0;
-    ssize_t inputRead;
-    int i;
+	char *input = NULL;
+	char *args[64] = { NULL };
+	size_t inputSize = 0;
+	ssize_t inputRead;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-        {
-            printf("$ ");
-            fflush(stdout);
-        }
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			printf("$ ");
+			fflush(stdout);
+		}
 
-        inputRead = getline(&input, &inputSize, stdin);
-        if (inputRead == EOF)
-        {
-            free(input);
-            exit(0);
-        }
+		inputRead = getline(&input, &inputSize, stdin);
+		if (inputRead == EOF)
+		{
+			free(input);
+			exit(0);
+		}
 
-        if (inputRead > 0 && input[inputRead - 1] == '\n')
-            input[inputRead - 1] = '\0';
-        if (spacesCheck(input) != 1)
-        {
-            tokenize(input, args);
-
-            if (strcmp(args[0], "/bin/ls") == 0 && args[1] == NULL)
-            {
-                for (i = 0; i < 3; i++)
-                {
-                    exec(args, input);
-                }
-                continue;
-            }
-        }
-    }
-    free(input);
-    return 0;
+		if (inputRead > 0 && input[inputRead - 1] == '\n')
+			input[inputRead - 1] = '\0';
+		if (spacesCheck(input) != 1)
+			tokenize(input, args);
+	}
+	free(input);
+	return (0);
 }
 
