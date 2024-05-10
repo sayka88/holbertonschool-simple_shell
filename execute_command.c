a46bc5892f0a511385b79d5a7046f6bd5673cde6
AysuAnar <8921@holbertonstudents.com>
2024-05-10 10:53:30 +0000

    add execute_command.c fix betty style

---
+++ execute_command.c
@@ -0,0 +1,34 @@
+#ifndef EXECUTE_COMMAND_H
+#define EXECUTE_COMMAND_H
+
+#include "main.h"
+
+/**
+* execute_command - Executes the command entered by the user.
+* @command_array: An array containing the command and its arguments.
+*
+* This function executes the command specified
+* by the user along with its arguments
+*
+* Return: The exit status of the executed command.
+*/
+
+int execute_command(char **command_array)
+{
+	pid_t pid;
+	int return_code;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		execve(command[0], command, environment_variables);
+		perror("execve failed");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(&return_code);
+		return (WEXITSTATUS(return_code));
+	}
+}
+gendif
