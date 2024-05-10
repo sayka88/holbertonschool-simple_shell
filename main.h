4ddbff7462b213529e2a59443bc4affc2a2440b8
AysuAnar <8921@holbertonstudents.com>
2024-05-10 10:53:08 +0000

    add main.h fix betty style

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
-#ifndef MAINH
-#define MAINH
-#define PATH "PATH="
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,15 +10,20 @@
 #include <sys/stat.h>
 #include <errno.h>
 
-extern char **path_list;
-extern char **environ;
-char **fetch_command(char **);
-char **initialize_path(char **);
-char *search_file(char *file_name, int *);
+extern char **path_variables;
+extern char **environment_variables;
+
+char **_strtok(char *str, char delim);
+int exec_command(char **command);
+char *find_file_location(char *command, int *lookup);
+int count_arguments(char *input_buffer, int size);
+char **parse_command(char **input_buffer);
+void search_path(char **env_vars, int *row_index, int *column_index);
+char **initialize_path(char **environment_variables);
+void release_path_memory(void);
 void print_environment(void);
 void print_prompt(int status);
-void release_path_memory(void);
-int execute_command(char **command_array);
-char **custom_strtok(char *str, char delim);
+int shell_main_helper(char **command_array, int status, char *input_buffer);
+int start_shell(int argc, char **argv, char **environment);
 
 #endif
