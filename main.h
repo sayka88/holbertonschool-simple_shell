3e41921e58eadc00cba7efd37d69c5131d32f115
sayka88 <sayka.alekperova@gmail.com>
2024-05-10 16:42:47 +0000

    main.h fix betty style

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef MAINH
+#define MAINH
+#define PATH "PATH="
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,22 +10,15 @@
 #include <sys/stat.h>
 #include <errno.h>
 
-extern char **path_variables;
-extern char **environment_variables;
-
-char **custom_strtok(char *str, char delim);
-int execute_command(char **command_array);
-char *find_file_location(char *file_name, int *lookup);
-int count_arguments(char *input_buffer, int size);
-char **fetch_command(char **input_buffer);
-
-char **parse_command(char **input_buffer);
-void search_path(char **env_vars, int *row_index, int *column_index);
-char **initialize_path(char **environment_variables);
-void release_path_memory(void);
-void print_environment(void);
+extern char **path_var;
+extern char **environ;
+char **get_command(char **);
+char **get_path(char **);
+char *find_file(char *command, int *);
+void print_env(void);
 void print_prompt(int status);
-int shell_main_helper(char **command_array, int status, char *input_buffer);
-int start_shell(int argc, char **argv, char **environment);
+void free_path(void);
+int exec_c(char **fcommand);
+char **_strtok(char *str, char delim);
 
 #endif
